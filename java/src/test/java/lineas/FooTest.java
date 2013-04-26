709bb6244a93f4ed2e79442f3508cd7eb35b61f9
Sponiro <sponiro@googlemail.com>
2013-04-26 11:06:59 +0200

    added java example

---
+++ java/src/test/java/lineas/FooTest.java
@@ -0,0 +1,14 @@
+package lineas;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ */
+public class FooTest {
+    
+    @Test
+    public void testMe() {
+//       Assert.fail();
+    }
+}
